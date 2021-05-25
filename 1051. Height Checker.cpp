bdb8c15d86bd599b38d4b7a142043fc2618c8d88
Gurudev singh <gurudev620.gs@gmail.com>
2021-05-25 19:11:31 +0530

    1051

---
+++ 1051. Height Checker.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& h) {
+        int n=h.size();
+        vector<int>temp(n);
+        for(int i=0;i<h.size();i++)
+        {
+           temp[i]=h[i];
+        }
+        sort(temp.begin(),temp.end());
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(h[i]!=temp[i])
+                count++;
+        }
+        return count;
+    }
+};
