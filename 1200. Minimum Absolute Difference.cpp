9f84624afb5f82e95385a153e392a4fbf22124a3
Gurudev singh <gurudev620.gs@gmail.com>
2021-05-30 14:58:16 +0530

    1200

---
+++ 1200. Minimum Absolute Difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& a) {
+        sort(a.begin(),a.end());
+        int n=a.size();
+        int diff=INT_MAX;
+        vector<vector<int>>ans;
+        for(int i=1;i<n;i++)
+        {   
+            if(a[i]-a[i-1]<diff){
+                ans.clear();
+                ans.push_back({a[i-1],a[i]});
+                diff=a[i]-a[i-1];
+            }
+            else if(a[i]-a[i-1]==diff)
+                ans.push_back({a[i-1],a[i]});
+        }
+        return ans;
+    }
+};
